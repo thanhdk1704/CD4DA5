f9262223d0dec19845adfca65e17b3f789f82970
dickdzaii <70245065+dickdzaii@users.noreply.github.com>
2020-10-06 20:31:28 +0700

    update emotion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,20 +2,23 @@ import { Component, OnInit, AfterViewInit, Injector } from '@angular/core';
 import { BaseComponent } from '../services/base.component';
 import { Observable } from 'rxjs';
 import {  combineLatest } from 'rxjs';
+import 'rxjs/add/operator/takeUntil';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent extends BaseComponent implements OnInit {
-
+  menus:any; 
   list_item:any;
   constructor(injector: Injector) { 
     super(injector);
   }
  
   ngOnInit(): void {
-    
+    this._api.get('api/Loai/all-with-children').takeUntil(this.unsubscribe).subscribe(res => {
+      this.menus = res;
+    }); 
     // Observable.combineLatest(
     //   this._api.get('/api/item/get-all'),
     // ).takeUntil(this.unsubscribe).subscribe(res => {
